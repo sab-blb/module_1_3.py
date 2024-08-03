a4621e3b9924e81f3c196dcefdd98e91600697d4
Alexandr <alexandr_balabanov55@mail.ru>
2024-08-03 18:35:22 +0500

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = "Name: Alexandr"
+print(name, type(name))
+age = 28
+print(age, type(age))
+new_age = age + 1
+print(new_age, type(new_age))
+is_student = True
+print(is_student, type(is_student))
